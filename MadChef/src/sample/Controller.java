c4b11b0cb4f8cadbef430cc29738a4bfb5de1733
Adiba Hasan <56604392+adibahasan11@users.noreply.github.com>
2020-12-22 17:12:29 +0600

    HomePage-PrimaryUI

---
+++ MadChef/src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void addStaff(ActionEvent event){
+    }
+
+    public void editStaff(ActionEvent event){
+    }
+
+    public void editMenu(ActionEvent event){
+    }
+
+    public void viewMenu(ActionEvent event){
+    }
+
+    public void takeOrder(ActionEvent event){
+    }
+
+    public void editInfo(ActionEvent event){
+    }
+}
