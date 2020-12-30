938a9693a4de8001a2458ab729c2d9e0803563b3
Sananda <sananda@iut-dhaka.edu>
2020-12-30 23:09:28 +0600

    updated

--- MADchef/src/sample/Controller.java
+++ MADchef/src/sample/Controller.java
@@ -8,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -21,7 +22,7 @@ public class Controller {
     TextField txt_mail;
 
     @FXML
-    TextField txt_pass;
+    PasswordField txt_pass;
 
     @FXML
     Label errorLbl;
